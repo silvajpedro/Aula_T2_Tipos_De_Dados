762f5bb035e3ce2f27cd4b73510c4f89f35837b0
silvajpedro <belojpedro.31@gmail.com>
2025-01-31 21:19:45 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,49 @@
+# 📌 Variáveis  
+# São como caixinhas que armazenam informações para serem usadas no código.  
+# Em Python, NÃO existe `var`, `let` ou `const` como no JavaScript.  
+# Basta dar um nome à variável e atribuir um valor a ela.  
+
+# 🔹 Convenções de Nomeação de Variáveis  
+# CamelCase → Estilo mais usado no JavaScript  
+pratoDaSemana = "picanha ao molho gorgonzola com fritas"  
+
+# Snake_Case → Padrão mais comum no Python  
+prato_da_semana = "Escondidinho de Macaxeira com Carne de Sol"  
+
+prato_da_semana = 3.14  # O Python permite reatribuir variáveis com tipos diferentes  
+
+# 📌 Tipos de Dados em Python  
+
+# 🔹 Inteiros (int)  
+# Representam números inteiros (positivos ou negativos), sem ponto decimal.  
+preco_escondidinho = 20  
+divida_fernanda = -100  
+
+# 🔹 Ponto Flutuante ou Decimal (float)  
+# Representam números que possuem casas decimais.  
+preco_picanha = 39.90  
+pi = -3.14  
+
+# 🔹 Strings (str)  
+# Sequências de caracteres envolvidas por aspas simples ('') ou duplas ("").  
+prato_dia = "Lasanha de Camarão com Catupiry com 3 acompanhamentos"  
+numero_da_sorte = "7"  # Mesmo sendo um número, está dentro de aspas, então é uma string  
+
+# 🔹 Booleanos (bool)  
+# Representam valores lógicos: Verdadeiro (True) ou Falso (False).  
+maior_de_idade = True  
+conta_tem_saldo = False  
+
+# 📌 Verificando o tipo de dado usando `type()`  
+print(type(conta_tem_saldo))  # Saída: <class 'bool'>  
+
+# 🔹 Nulos (None)  
+# Representam ausência de valor ou variáveis ainda não inicializadas.  
+valor_nulo = None  
+
+# Verificando o tipo da variável  
+print(type(maior_de_idade))  # Saída: <class 'bool'>  
+
+# 📌 Operações Matemáticas  
+primeiro_valor = 5.89  
+segundo_valor = 13.7895
